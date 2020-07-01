3ab48374d400276ac5b2639081d4b1ca8b965aca
채정실 <cowjdtlf@gmail.com>
2020-07-01 21:51:53 +0900

    Initial commit

---
+++ app/src/main/java/kr/co/tjoeun/loginpractice_20200701/MainActivity.kt
@@ -0,0 +1,30 @@
+package kr.co.tjoeun.loginpractice_20200701
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        loginBtn.setOnClickListener {
+
+            val id = inputId.text.toString()
+            val pw = inputPw.text.toString()
+
+            if (id == "admin@test.com" && pw == "qwer1234") {
+                Toast.makeText(this, "관리자입니다.", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(this, "로그인 실패", Toast.LENGTH_SHORT).show()
+
+            }
+
+
+        }
+
+
+    }
+}
